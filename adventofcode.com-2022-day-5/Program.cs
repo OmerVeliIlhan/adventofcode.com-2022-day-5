a363e36a58b8da4253ec8c3fd8f81de57402bfe3
Omer Veli İlhan <omerv.ilhan@hotmail.com>
2022-12-06 14:18:15 +0300

    Initial commit

---
+++ adventofcode.com-2022-day-5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adventofcode.com_2022_day_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
